e06a7d9d9676ed112ab08c14efaa334c68d2db36
prampal <93888290+prampal@users.noreply.github.com>
2026-01-28 23:19:22 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -26,15 +26,26 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in[0].value = 0 # d
+    dut.ui_in[1].value = 0 # e
 
+    # Start toggling d and e
+    cocotb.start_soon(toggle_signal(dut, index=0, period_ns=50)) #d = ui_in[0]
+    cocotb.start_soon(toggle_signal(dut, index=1, period_ns=30)) #e = ui_in[1
+    
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 1000)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    #assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+async def toggle_signal(dut, index, period_ns):
+"""Toggle ui_in[index] every period_ns nanoseconds."""
+dut.ui_in[index].value = 0
+while True:
+await Timer(period_ns, units="ns")
+dut.ui_in[index].value = not dut.ui_in[index].value
